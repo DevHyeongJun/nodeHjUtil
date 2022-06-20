6700e6868222f246abcaac4240927a63b1adeb71
AHJ <ahj5229@gmail.com>
2022-06-20 12:13:53 +0900

    svn 빌드 개발 완료-maven 빌드 가능 시

---
+++ batt.bat
@@ -0,0 +1,6 @@
+set MAVEN_PATH=%cd%/modules/maven/bin
+
+
+cd %1
+
+%MAVEN_PATH%/mvn -U clean --update-snapshots dependency:copy-dependencies package -Dmaven.text.skip=true -Dmaven.text.skip=true
--- work/svnWork.js
+++ work/svnWork.js
@@ -1,14 +1,62 @@
 /**
  * SVN의 최신 정보를 받아와 war로 반환한다.
- * 
+ *  - maven 빌드 환경이 구축되어야 가능하다.....................
  */
 const GlobalConfig = require('../config.js');
-const svnUltimate = require('node-svn-ultimate');
+const { exec, execSync } = require("child_process");
 
-/**
- * svn 체크 아웃
- */
-svnUltimate.commands.export( 'http://10.1.73.15:18080/svn/xeus/openapi/', 'd:/svnWork/', ( err ) => {
-    console.log(err);
-    console.log( "Checkout complete" );
-});
+const fs = require("fs-extra");
+
+var svnUrl = 'http://10.1.73.15:18080/svn/xeus-smartcity/xeus-smartcity-seoul-police/public/xeus-seoul-police-public/';
+var targetDir = 'D:/work';
+
+var command = `svn checkout ${svnUrl} ${targetDir}`;
+
+const initWorkDir = () => {
+    //폴더
+    if ( fs.emptyDirSync(targetDir) ) {
+        fs.removeSync(targetDir);
+        console.log('123');
+        fs.mkdirSync(targetDir);
+    }
+}
+
+const execSvnCheckOut = () => {
+    execSync(command, {stdio: 'inherit'}, (error, stdout, stderr) => {
+
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+
+        console.log(`stdout: ${stdout}`);
+    });
+}
+
+const execMavenBuild = () => {
+    execSync('batt.bat d:/work', {stdio: 'inherit'}, (error, stdout, stderr) => {
+
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+
+        console.log(`stdout: ${stdout}`);
+    });
+}
+
+//initWorkDir();
+//execSvnCheckOut();
+execMavenBuild();
+
+
+
+
